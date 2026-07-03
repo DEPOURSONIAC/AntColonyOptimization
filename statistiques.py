d974f41ff7878e0b1cc60e9ac47e052e8e876f0b
DEPOUROSNIAC <DEPOURSONIAC.JEAN@outlook.com>
2026-07-03 21:26:19 +0200

    Add files via upload

---
+++ statistiques.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import csvGestion
+import moduleAco
+
+# je teste une config -> je lance plusieurs fois -> je fais une moyenne -> je stocke
+
+
+
+if (__name__ == "__main__"):
+    print('helloWorld!')
